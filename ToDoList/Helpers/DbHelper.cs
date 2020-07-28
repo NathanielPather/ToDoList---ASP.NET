de4ccc3efba18edc047e0dac7c0748b7df90dfb7
Nathaniel Pather <nathaniel_pather@hotmail.com>
2020-07-28 22:54:38 +1000

    Add project files.

---
+++ ToDoList/Helpers/DbHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ToDoList.Helpers
+{
+    public class DbHelper
+    {
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(@"Data Source=(localdb)\ProjectsV13;Initial Catalog=ToDoList;");
+        }
+    }
+}
---
+++ ToDoList/ViewModels/ToDoListViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ToDoList.Models;
+using Dapper;
+using ToDoList.Helpers;
+using System.ComponentModel.Design;
+
+namespace ToDoList.ViewModels
+{
+    public class ToDoListViewModel
+    {
+        public ToDoListViewModel()
+        {
+            using(var db = DbHelper.GetConnection())
+            {
+                this.EditableItem = new ToDoListItem();
+                this.ToDoItems = db.Query<ToDoListItem>("SELECT * FROM ToDoListItems ORDER BY AddDate DESC").ToList();
+            }
+        }
+
+        public List<ToDoListItem> ToDoItems { get; set; }
+
+        public ToDoListItem EditableItem { get; set; }
+    }
+}
